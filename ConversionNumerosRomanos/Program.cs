60128e1c7491f97797512513b622c9749146c98b
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-02 00:55:17 -0400

    Terminacion de programa

--- ConversionNumerosRomanos/Program.cs
+++ ConversionNumerosRomanos/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello ");
+﻿using System;
+
+
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un numero romano");
+        string romano = Console.ReadLine().ToUpper();
+
+        int total = 0;
+
+        for(int i = 0; i < romano.Length; i++)
+        {
+            int valor = ValorRomano(romano[i]);
+            if(i + 1 < romano.Length && ValorRomano(romano[i + 1]) > valor)
+            {
+                total -= valor;
+            }
+            else
+            {
+                total += valor;
+            }
+        }
+        Console.WriteLine("El numero decimal es: " + total);
+    }
+
+    static int ValorRomano(char c)
+    {
+        if(c == 'I') 
+            return 1;
+        if(c == 'V')
+            return 5;
+        if(c == 'X')
+            return 10;
+        if(c == 'L')
+            return 50;
+        if(c == 'C')
+            return 100;
+        if(c == 'D')
+            return 500;
+        if(c == 'M')
+            return 1000;
+        return 0;
+    }
+}
